a2347284b7c455a1e2c3b0ab62f49865b6dc8a78
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-11-23 15:59:13 +0330

    Week3 Challenge2&3

---
+++ Week3/HackerRank_ThreeMonthsPrep_Week3/Subarray_Division_2/Program.cs
@@ -0,0 +1,48 @@
+﻿class Result
+{
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int sum = 0; int result = 0;
+        for (int i = 0; i < m; i++)
+        {
+            sum += s[i];
+        }
+        if (sum == d)
+            result++;
+
+        for (int i = 0; i < s.Count - m; i++)
+        {
+            sum -= s[i];
+            sum += s[i + m];
+            if (sum == d)
+                result++;
+        }
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week3/HackerRank_ThreeMonthsPrep_Week3/XOR_Strings_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Result
+    {
+        public static string XOR_String(string first, string second)
+        {
+            char[] result = new char[first.Length];
+            for (int i = 0; i < first.Length; i++)
+                result[i] = (first[i] == second[i]) ? '0' : '1';
+            return new string(result);
+        }
+    }
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string first_line = Console.ReadLine();
+            string second_line = Console.ReadLine();
+
+            Console.Write(Result.XOR_String(first_line, second_line));
+        }
+    }
+}
