3509e3a67f9fc977b70952ea59b882d3f628ef03
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-06 14:57:20 +0330

    challenge 4&5

---
+++ Week4/HackerRank_ThreeMonthsPrep_Week4/Closest_Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿class Result
+{
+    public static List<int> closestNumbers(List<int> arr)
+    {
+        int min_defference = int.MaxValue; List<int> result = new List<int>();
+        arr.Sort();
+        
+        for (int i = arr.Count - 1; i > 0; i--)
+        {
+            if (arr[i] - arr[i - 1] < min_defference)
+                min_defference = arr[i] - arr[i - 1];
+        }
+
+        for (int i = 0; i < arr.Count - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] == min_defference)
+            {
+                result.Add(arr[i]);
+                result.Add(arr[i + 1]);
+            }
+        }
+
+        return result;
+    }
+}
+//First we sort the array because after sorting, the smallest absolute difference can only occur between neighboring elements. By doing this we avoid checking all possible pairs (O(n^2)) and only compare adjacent pairs (O(n)).
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.closestNumbers(arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week4/HackerRank_ThreeMonthsPrep_Week4/Separate_the_Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿class Result
+{
+    public static void separateNumbers(string s)
+    {
+        for (int i = 1; i <= s.Length / 2; i++)
+        {
+            string first_part = s.Substring(0, i);
+            if (first_part[0] == '0')
+                break;
+
+            string buildstring = first_part;
+            long first_part_num = long.Parse(first_part);
+
+            int j = 1;
+            while (buildstring.Length < s.Length)
+            {
+                buildstring += (first_part_num + j).ToString();
+                j++;
+            }
+
+            if (buildstring == s)
+            {
+                Console.WriteLine($"YES {first_part}");
+                return;
+            }
+        }
+        Console.WriteLine("NO");
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            Result.separateNumbers(s);
+        }
+    }
+}
