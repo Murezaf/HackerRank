75896c19bdca2e16145d618d655e09d1dc55f2cd
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-13 10:04:16 +0330

    challenge 1&2 of week 5

---
+++ Week5/HackerRank_ThreeMonthsPrep_Week5/Max_Min/Program.cs
@@ -0,0 +1,44 @@
+﻿class Result
+{
+    public static int maxMin(int k, List<int> arr)
+    {
+        arr.Sort(); int minUnfairness = int.MaxValue; int unfairness = 0;
+
+        for (int i = 0; i < arr.Count - k + 1; i++)
+        {
+            unfairness = arr[i + k - 1] - arr[i];
+
+            if (unfairness < minUnfairness)
+                minUnfairness = unfairness;
+        }
+
+        return minUnfairness;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+            arr.Add(arrItem);
+        }
+
+        int result = Result.maxMin(k, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week5/HackerRank_ThreeMonthsPrep_Week5/Strong_Password/Program.cs
@@ -0,0 +1,50 @@
+﻿class Result
+{
+    public static int minimumNumber(int n, string password)
+    {
+        Dictionary<string, bool> conditions = new Dictionary<string, bool>(); int countCharacterNeed = 0;
+        List<char> specialCharacters = new List<char>() { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' };
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!conditions.ContainsKey("lowercase") && (password[i] >= 97 && password[i] <= 122))
+                conditions["lowercase"] = true;
+
+            else if (!conditions.ContainsKey("uppercase") && (password[i] >= 65 && password[i] <= 90))
+                conditions["uppercase"] = true;
+
+            else if (!conditions.ContainsKey("numbers") && int.TryParse(password[i].ToString(), out int x))
+                conditions["number"] = true;
+
+            else if (!conditions.ContainsKey("specialcharacter") && specialCharacters.Contains(password[i]))
+                conditions["specialcharacter"] = true;
+        }
+
+        countCharacterNeed = 4 - conditions.Count;
+        n += 4 - conditions.Count;
+
+        if(n < 6)
+            countCharacterNeed += (6 - n);
+
+        return countCharacterNeed;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string password = Console.ReadLine();
+
+        int answer = Result.minimumNumber(n, password);
+
+        textWriter.WriteLine(answer);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
