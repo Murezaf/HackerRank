84ee5fb18950342a25d27e171fca52cd1d50d383
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-19 13:03:57 +0330

    Challenges 6&7

---
+++ Week5/HackerRank_ThreeMonthsPrep_Week5/Grid_Challenge/Program.cs
@@ -0,0 +1,57 @@
+﻿class Result
+{
+    public static string gridChallenge(List<string> grid)
+    {
+        int numberOfRows = grid.Count; int numberOfColumns = grid[0].Length;
+
+        //Sorting rows
+        for(int i = 0; i < numberOfRows; i++)
+        {
+            char[] charactersOfRow = grid[i].ToArray();
+            Array.Sort(charactersOfRow);
+            grid[i] = new string(charactersOfRow);
+        }
+
+        //Checking for order in columns
+        for (int column = 0; column < numberOfColumns; column++)
+        {
+            for(int row = 0; row < numberOfRows-1; row++)
+            {
+                if (grid[row][column] > grid[row + 1][column])
+                    return "NO";
+            }
+        }
+
+        return "YES";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> grid = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string gridItem = Console.ReadLine();
+                grid.Add(gridItem);
+            }
+
+            string result = Result.gridChallenge(grid);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week5/HackerRank_ThreeMonthsPrep_Week5/The_Full_Counting_Sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Immutable;
+using System.Text;
+
+class Result
+{
+    public static void countSort(List<List<string>> arr)
+    {
+        Dictionary<int, StringBuilder> map = new Dictionary<int, StringBuilder>(); StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < arr.Count / 2; i++)
+            arr[i][1] = "-";
+
+        foreach (var pair in arr)
+        {
+            int key = int.Parse(pair[0]);
+            string value = pair[1];
+
+            if (!map.ContainsKey(key))
+                map[key] = new StringBuilder();
+
+            map[key].Append(value);
+            map[key].Append(' ');
+        }
+
+        //var sortedMap = map.OrderBy(x => x.Key);
+        ////IOrderedEnumerable<KeyValuePair<int, StringBuilder>> sortedMap = map.OrderBy(x => x.Key);
+
+        //foreach (KeyValuePair<int, StringBuilder> pair in sortedMap)
+        //    result.Append(pair.Value);
+
+        List<int> keys = new List<int>(map.Keys);
+        keys.Sort();
+
+        foreach (var key in keys)
+            result.Append(map[key]);
+
+        Console.WriteLine(result.ToString().Trim());
+    }
+}
+//Using StringBuilder for efficient string concatenation.
+//Direct 'string +=' operations are slow due to string immutability (a new object of string creates each time). Leading to performance issues like Time Limit Exceeded (TLE) for large data.
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<string>> arr = new List<List<string>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
+        }
+
+        Result.countSort(arr);
+    }
+}
