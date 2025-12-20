3e1f2deb4265f10b98e3f5ee8ebb577a261a0600
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-20 12:45:57 +0330

    first two challenges of week 6

---
+++ Week6/HackerRank_ThreeMonthsPrep_Week6/Prime_Dates/Program.cs
@@ -0,0 +1,109 @@
+﻿//This was a debugging challenge: commented lines had logical issues during debugging process and were corrected in their following lines.
+using System.Text.RegularExpressions;
+
+class Solution
+{
+    static List<int> month = new List<int>();
+
+    static void updateLeapYear(int year)
+    {
+        if (year % 400 == 0)
+        {
+            //month[2] = 28;
+            month[2] = 29;
+        }
+        else if (year % 100 == 0)
+        {
+            //month[2] = 29;
+            month[2] = 28;
+        }
+        else if (year % 4 == 0)
+        {
+            month[2] = 29;
+        }
+        else
+        {
+            month[2] = 28;
+        }
+    }
+
+    static void storeMonth()
+    {
+        month[1] = 31;
+        month[2] = 28;
+        month[3] = 31;
+        month[4] = 30;
+        month[5] = 31;
+        month[6] = 30;
+        month[7] = 31;
+        month[8] = 31;
+        month[9] = 30;
+        month[10] = 31;
+        month[11] = 30;
+        month[12] = 31;
+    }
+
+    static int findPrimeDates(int d1, int m1, int y1, int d2, int m2, int y2)
+    {
+        storeMonth();
+        int numberOfLuckyDates = 0;
+
+        while (true)
+        {
+            int x = d1;
+            x = x * 100 + m1;
+            //x = x * 1000 + y1;
+            x = x * 10000 + y1;
+
+            //if (x % 4 == 0 && x % 7 == 0)
+            if (x % 4 == 0 || x % 7 == 0)
+            {
+                numberOfLuckyDates = numberOfLuckyDates + 1;
+            }
+
+            if (d1 == d2 && m1 == m2 && y1 == y2)
+            {
+                break;
+            }
+
+            updateLeapYear(y1);
+            d1 = d1 + 1;
+
+            if (d1 > month[m1])
+            {
+                m1 = m1 + 1;
+                d1 = 1;
+
+                if (m1 > 12)
+                {
+                    y1 = y1 + 1;
+                    //m1 = m1 + 1;
+                    m1 = 1;
+                }
+            }
+        }
+
+        return numberOfLuckyDates;
+    }
+
+    static void Main()
+    {
+        for (int i = 1; i < 15; i++)
+        {
+            month.Add(31);
+        }
+
+        string line = Console.ReadLine();
+        string[] date = Regex.Split(line, "-| ");
+
+        int d1 = int.Parse(date[0]);
+        int m1 = int.Parse(date[1]);
+        int y1 = int.Parse(date[2]);
+        int d2 = int.Parse(date[3]);
+        int m2 = int.Parse(date[4]);
+        int y2 = int.Parse(date[5]);
+
+        int result = findPrimeDates(d1, m1, y1, d2, m2, y2);
+        Console.WriteLine(result);
+    }
+}
---
+++ Week6/HackerRank_ThreeMonthsPrep_Week6/Sherlock_and_Array/Program.cs
@@ -0,0 +1,46 @@
+﻿class Result
+{
+    public static string balancedSums(List<int> arr)
+    {
+        int temp = 0; int sum = 0;
+
+        foreach (int i in arr)
+            sum += i;
+
+        if (sum - arr[0] == 0 || sum - arr[arr.Count - 1] == 0)
+            return "YES";
+
+        for (int i = 0; i < arr.Count - 1; i++)
+        {
+            temp += arr[i];
+            if (temp == sum - temp - arr[i + 1])
+                return "YES";
+        }
+
+        return "NO";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int T = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int TItr = 0; TItr < T; TItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = Result.balancedSums(arr);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
