c98552d26c7527b77409a87cde3266b1284c9a0e
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-21 14:06:26 +0330

    Challenges 3&4

---
+++ Week6/HackerRank_ThreeMonthsPrep_Week6/Gaming_Array_1/Program.cs
@@ -0,0 +1,45 @@
+﻿class Result
+{
+    public static string gamingArray(List<int> arr)
+    {
+        int numberOfRemovals = 0; int max = int.MinValue;
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                numberOfRemovals++;
+            }
+        }
+
+        if (numberOfRemovals % 2 == 1)
+            return "BOB";
+        return "ANDY";
+    }
+}
+//The game ends when the maximum element of the list becomes the first element. We can determine how many moves are played until the game reaches its ending condition.
+ 
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int gItr = 0; gItr < g; gItr++)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = Result.gamingArray(arr);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week6/HackerRank_ThreeMonthsPrep_Week6/MisereـNim/Program.cs
@@ -0,0 +1,50 @@
+﻿class Result
+{
+    public static string misereNim(List<int> s)
+    {
+        bool allOne = true; int finalXOR = 0;
+        for (int i = 0; i < s.Count; i++)
+        {
+            if (s[i] != 1)
+                allOne = false;
+
+            finalXOR ^= s[i];
+        }
+
+        if(allOne)
+        {
+            if (s.Count % 2 == 0)
+                return "First";
+            return "Second";
+        }
+
+        if (finalXOR == 0)
+            return "Second";
+
+        return "First";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+            string result = Result.misereNim(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
