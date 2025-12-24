a3e1bd04f5b51d927bf206816c0caa2e687d9ba2
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-24 17:00:10 +0330

    implement Counter game & Recursive Digit Sum

---
+++ Week6/HackerRank_ThreeMonthsPrep_Week6/Counter_game/Program.cs
@@ -0,0 +1,94 @@
+﻿class Result
+{
+    public static string counterGame(long n)
+    {
+        int turn = 0; long closestPowerOfTwo = 1;
+
+        if (n == 1)
+            return "Richard";
+
+        while (n != 1)
+        {
+            if ((n & (n - 1)) == 0)
+                n = n / 2;
+
+            else
+            {
+                while(closestPowerOfTwo * 2 < n)
+                {
+                    closestPowerOfTwo *= 2;
+                }
+                n -= closestPowerOfTwo;
+                closestPowerOfTwo = 1;
+            }
+
+            turn++;
+        }
+
+        if (turn % 2 == 1)
+            return "Louise";
+
+        return "Richard";
+    }
+
+
+
+    //public static string counterGame(long n)
+    //{
+    //    int turn = 0;
+
+    //    if (n == 1)
+    //        return "Richard";
+
+    //    while (n != 1)
+    //    {
+    //        int powerOfTwo = FindClosestPowerOfTwo(n);
+    //        if (Math.Pow(2, powerOfTwo) == n)
+    //            n = n / 2;
+    //        else
+    //            n -= (long)Math.Pow(2, powerOfTwo);
+
+    //        turn++;
+    //    }
+
+    //    if (turn % 2 == 1)
+    //        return "Louise";
+
+    //    return "Richard";
+    //}
+
+    //public static int FindClosestPowerOfTwo(long n)
+    //{
+    //    int count = 0;
+
+    //    while (n != 1)
+    //    {
+    //        n = n / 2;
+    //        count++;
+    //    }
+
+    //    return count;
+    //}
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+            string result = Result.counterGame(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week6/HackerRank_ThreeMonthsPrep_Week6/Recursive_Digit_Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿class Result
+{
+    public static int superDigit(string n, int k)
+    {
+        long sumOfDigits = 0; long temp = 0;
+        
+        for(int i  = 0; i < n.Length; i++)
+            sumOfDigits += n[i] - '0';
+        sumOfDigits *= k;
+
+        while(sumOfDigits >= 10)
+        {
+            while(sumOfDigits > 0)
+            {
+                temp += sumOfDigits % 10;
+                sumOfDigits /= 10;
+            }
+
+            sumOfDigits = temp;
+            temp = 0;
+        }
+
+        return (int)sumOfDigits;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        string n = firstMultipleInput[0];
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
