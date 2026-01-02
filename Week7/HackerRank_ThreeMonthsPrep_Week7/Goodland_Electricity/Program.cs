fb5b8cef537759b17717368964d404f552b42886
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-01-03 02:16:02 +0330

    GoodlandElectricity & SherlockValidString challenges

---
+++ Week7/HackerRank_ThreeMonthsPrep_Week7/Goodland_Electricity/Program.cs
@@ -0,0 +1,51 @@
+﻿class Result
+{
+    public static int pylons(int k, List<int> arr)
+    {
+        int count = 0; int currentIndex = 0; bool foundPlant = false;
+
+        while (currentIndex < arr.Count)
+        {
+            foundPlant = false;
+
+            for (int farestPowerPlantPossible = Math.Min(arr.Count - 1, currentIndex + k - 1); farestPowerPlantPossible >= Math.Max(0, currentIndex - (k - 1)); farestPowerPlantPossible--)
+            {
+                if (arr[farestPowerPlantPossible] == 1)
+                {
+                    count++;
+                    foundPlant = true;
+                    currentIndex = farestPowerPlantPossible + k;
+                    break;
+                }
+            }
+
+            if (!foundPlant)
+                return -1;
+        }
+
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.pylons(k, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Week7/HackerRank_ThreeMonthsPrep_Week7/Sherlock_and_the_Valid_String/Program.cs
@@ -0,0 +1,56 @@
+﻿class Result
+{
+    public static string isValid(string s)
+    {
+        Dictionary<char, int> numberOfEachCharacter = new Dictionary<char, int>();
+
+        foreach (char character in s)
+        {
+            if (numberOfEachCharacter.ContainsKey(character))
+                numberOfEachCharacter[character]++;
+            else
+                numberOfEachCharacter[character] = 1;
+        }
+
+        List<int> listOfCharacterNumbers = numberOfEachCharacter.Values.ToList();
+        Dictionary<int, int> occurrenceCount = new Dictionary<int, int>();
+
+        foreach (int number in listOfCharacterNumbers)
+        {
+            if(occurrenceCount.ContainsKey(number))
+                occurrenceCount[number]++;
+
+            else occurrenceCount[number] = 1;
+        }
+
+        if (occurrenceCount.Count == 1) return "YES";
+        if (occurrenceCount.Count == 2)
+        {
+            int min = listOfCharacterNumbers.Min();
+            int max = listOfCharacterNumbers.Max();
+            
+            if (min == 1 && occurrenceCount[min] == 1) return "YES";
+
+            if (max == min + 1 && occurrenceCount[max] == 1) return "YES";
+        }
+
+        return "NO";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.isValid(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
