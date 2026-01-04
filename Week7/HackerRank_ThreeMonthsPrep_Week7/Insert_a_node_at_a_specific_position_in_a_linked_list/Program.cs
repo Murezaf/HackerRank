d405189fd9c8c8291318bd51e4bd76f6d92ec16c
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-01-04 14:31:56 +0330

    Insert a node at a specific position in a linked list challenge

---
+++ Week7/HackerRank_ThreeMonthsPrep_Week7/Insert_a_node_at_a_specific_position_in_a_linked_list/Program.cs
@@ -0,0 +1,107 @@
+﻿class SinglyLinkedListNode
+{
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData)
+    {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList
+{
+    public SinglyLinkedListNode head;
+    public SinglyLinkedListNode tail;
+
+    public SinglyLinkedList()
+    {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void InsertNode(int nodeData)
+    {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null)
+        {
+            this.head = node;
+        }
+        else
+        {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+}
+
+class Result
+{
+    public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+    {
+        SinglyLinkedListNode addedNode = new SinglyLinkedListNode(data); SinglyLinkedListNode current = llist;
+
+        if(position == 0 || llist == null)
+        {
+            addedNode.next = llist;
+            return addedNode;
+        }
+
+        for (int i = 0; i < position-1; i++)
+        {
+            current = current.next;
+        }
+        SinglyLinkedListNode temp = current.next;
+        current.next = addedNode;
+        addedNode.next = temp;
+
+        return llist;
+    }
+}
+
+public class Solution
+{
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++)
+        {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            llist.InsertNode(llistItem);
+        }
+
+        int data = Convert.ToInt32(Console.ReadLine());
+
+        int position = Convert.ToInt32(Console.ReadLine());
+
+        SinglyLinkedListNode llist_head = Result.insertNodeAtPosition(llist.head, data, position);
+
+        PrintSinglyLinkedList(llist_head, " ", textWriter);
+        textWriter.WriteLine();
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
